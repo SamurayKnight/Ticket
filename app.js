864ee729162cb0417e7ff2695ce8ca39e1b6097b
SamurayKnight <danlee97.06@gmail.com>
2022-10-01 11:36:05 -0500

    Second_commit

---
+++ app.js
@@ -0,0 +1,9 @@
+const $ = selector => document.querySelector(selector);
+
+const wrapper = $('.wrapper');
+const ticket = $('.ticket');
+
+wrapper.addEventListener('mousemove', event => {
+    const { offsetX, offsetY} = event
+    
+})
